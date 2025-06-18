720264656c5326b84662dd36b31e47e1607f416e
Emma <furlan.emmanuelle@neuf.fr>
2025-06-18 16:11:22 +0200

    users

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,3 +1,4 @@
+from app.models.users import User
 from app.persistence.repository import InMemoryRepository
 
 class HBnBFacade:
@@ -8,7 +9,26 @@ class HBnBFacade:
         self.amenity_repo = InMemoryRepository()
 
     def create_user(self, user_data):
-        pass
+        user = User(**user_data)
+        self.user_repo.add(user)
+        return user
+
+    def get_user(self, user_id):
+        return self.user_repo.get(user_id)
+
+    def get_user_by_email(self, email):
+        return self.user_repo.get_by_attribute('email', email)
+
+    def get_all_users(self):
+        return self.user_repo.all()
+
+    def update_user(self, user_id, update_data):
+        user = self.get_user(user_id)
+        if not user:
+            return None
+        for key, value in update_data.items():
+            setattr(user, key, value)
+        return user
 
     def get_place(self, place_id):
         pass
