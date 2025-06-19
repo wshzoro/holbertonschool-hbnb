ae9f9b33a0c456493914d8bb83c85bb4857b17fd
Najwarci <rachdi.najwa91@gmail.com>
2025-06-19 16:28:36 +0200

    add place

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,7 +1,7 @@
 from app.models.users import User
-from app.persistence.repository import repo
+from app.models.place import Place
 from app.models.amenity import Amenity
-from app.persistence.repository import InMemoryRepository
+from app.persistence.repository import repo, InMemoryRepository
 
 class HBnBFacade:
     def __init__(self):
@@ -32,8 +32,44 @@ class HBnBFacade:
             setattr(user, key, value)
         return user
 
+    def create_place(self, place_data):
+        required_fields = ['title', 'price', 'latitude', 'longitude', 'owner_id', 'amenities']
+        for field in required_fields:
+            if field not in place_data or not place_data[field]:
+                raise ValueError(f"{field} is required")
+
+        owner = repo.get(User, place_data['owner_id'])
+        if not owner:
+            raise ValueError("Invalid owner_id")
+
+        for amenity_id in place_data['amenities']:
+            if not repo.get(Amenity, amenity_id):
+                raise ValueError(f"Amenity not found: {amenity_id}")
+
+        place = Place(**place_data)
+        repo.save(place)
+        return place.to_dict()
+
     def get_place(self, place_id):
-        pass
+        place = repo.get(Place, place_id)
+        if not place:
+            raise ValueError("Place not found")
+        return place
+
+    def get_all_places(self):
+        return repo.all(Place)
+
+    def update_place(self, place_id, update_data):
+        place = repo.get(Place, place_id)
+        if not place:
+            raise ValueError("Place not found")
+
+        for key, value in update_data.items():
+            if hasattr(place, key):
+                setattr(place, key, value)
+
+        repo.save(place)
+        return place
 
     def create_amenity(self, amenity_data):
         if 'name' not in amenity_data or not amenity_data['name'].strip():
