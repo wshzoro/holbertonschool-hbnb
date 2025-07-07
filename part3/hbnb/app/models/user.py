dedb30139431a7a20ece6518d43c14c88acb6baa
Emma <furlan.emmanuelle@neuf.fr>
2025-07-07 12:18:50 +0200

    update

--- part3/hbnb/app/models/user.py
+++ part3/hbnb/app/models/user.py
@@ -1,23 +1,35 @@
 from .base_model import BaseModels
 from app import bcrypt
+import uuid
 
 class User(BaseModels):
-    def __init__(self, first_name, last_name, email, password, place_list=[], reviews=[], is_admin=False):
+    def __init__(self, first_name, last_name, email, password=None, is_admin=False):
         super().__init__()
+        self.id = str(uuid.uuid4())
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
-        self.password = None
+        self._password = None
         self.password_hash = None
         self.is_admin = is_admin
         self.place_list = []
         self.reviews = []
+        if password:
+            self.password = password
 
-    def password_hash(self, password):
-        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+    @property
+    def password(self):
+        return self._password
 
-    def check_password(self, password):
-        return bcrypt.check_password_hash(self.password, password)
+    @password.setter
+    def password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+        self._password = password
+
+    def verify_password(self, password):
+        if not self.password_hash or not password:
+            return False
+        return bcrypt.check_password_hash(self.password_hash, password)
 
     def register(self):
         self.save()
