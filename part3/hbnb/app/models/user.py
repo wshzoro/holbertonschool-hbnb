1f196e1033e254de86b593beb83e6df746075e2a
Emma <furlan.emmanuelle@neuf.fr>
2025-07-07 10:46:24 +0200

    update password hash

--- part3/hbnb/app/models/user.py
+++ part3/hbnb/app/models/user.py
@@ -1,4 +1,5 @@
 from .base_model import BaseModels
+from app import bcrypt
 
 class User(BaseModels):
     def __init__(self, first_name, last_name, email, password, place_list=[], reviews=[], is_admin=False):
@@ -6,16 +7,23 @@ class User(BaseModels):
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
-        self.password = password
+        self.password = None
+        self.password_hash = None
         self.is_admin = is_admin
         self.place_list = []
         self.reviews = []
+
+    def password_hash(self, password):
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password, password)
 
     def register(self):
         self.save()
     
     def delete(self):
         pass
-    
+
 
 
